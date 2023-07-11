579f68532bf437e0718db5f280e42a6f9597014e
Himanshu Sah <himanshusah41@gmail.com>
2023-07-11 00:56:04 -0400

    Login added

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -23,4 +23,24 @@ router.post('/register', async(req,res)=>{
     }
 });
 
+//login
+router.post('/login', async(req,res)=>{
+    try{
+        const user = await User.findOne({username:req.body.username});
+
+        if(user){
+            const validPassword = await bcrypt.compare(req.body.password, user.password);
+            if(validPassword){
+                res.status(200).json(user);
+            }else{
+                res.status(400).json("Wrong Password");
+            }
+        }else{
+            res.status(400).json("Username not Found!");
+        }
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
 module.exports = router;
