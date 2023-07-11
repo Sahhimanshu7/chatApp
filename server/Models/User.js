2c7eaed93e6080d53789852396a1728cb8aa09c6
Himanshu Sah <himanshusah41@gmail.com>
2023-07-10 22:22:42 -0400

    Server created, connected to database, and userSchema created

---
+++ server/Models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        min:3,
+        max:20,
+        unique:true
+    },
+    email:{
+        type:String,
+        require:true,
+        max:50,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        min:6
+    },
+    profilePicture:{
+        type:String,
+        default:" "
+    },
+    backgroundPicture:{
+        type:String,
+        default:" "
+    },
+    about:{
+        type:String,
+        max:160,
+        default:" "
+    },
+    location:{
+        type:String,
+        required:true,
+        max:20
+    },
+    organization:{
+        type:String,
+        default:" "
+    },
+    position:{
+        type:String,
+        max:40,
+        default:" "
+    },
+    hobbies:{
+        type:Array,
+        default:[],
+        max:9
+    }
+},
+{timestamps:true});
+
+module.exports = mongoose.model("User",UserSchema);
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+const fs = require('fs');
+dotenv.config();
+
+//Mongoose connection establishment and check
+mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser:true,useUnifiedTopology:true}).
+then(()=>{
+    console.log("Connected to database");
+}).catch(e=>{
+    console.log("Error ",e);
+})
+
+//middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
