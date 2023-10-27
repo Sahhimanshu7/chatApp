ecf113f858b61a2ec35caec42d47fd7cedad1db0
Himanshu Sah <himanshusah41@gmail.com>
2023-10-26 22:49:55 -0400

    Chats added

--- client/src/components/RightSide.jsx
+++ client/src/components/RightSide.jsx
@@ -7,7 +7,7 @@ import video from '../dummy images/video.png';
 import photo from '../dummy images/back.JPG';
 
 export default function RightSide({id}) {
-    console.log(id);
+    console.log(id.toString());
     // display the chat 
     // design the header body and footer 
 
@@ -16,6 +16,15 @@ export default function RightSide({id}) {
         "name": "Roji Lama",
         "userName": "ijor98"
     }
+
+    let prevId = " ";
+    const messages = [
+        {"sender": "1234", "content": "Hi"},
+        {"sender": "5678", "content": "Hello"},
+        {"sender": "5678", "content": "Do I know you?"},
+        {"sender": "1234", "content": "Yes you do."},
+        {"sender": "1234", "content": "We are great friends for a long time and we have done a lot of business together"}
+    ]
 
   return (
     <div className='right-side-comp'>
@@ -40,11 +49,46 @@ export default function RightSide({id}) {
             </div>
         </div>
         <div className='right-body'>
-
+            {messages.map((message) => {
+            
+            if(message.sender === id.toString() && (prevId === " " || prevId === "5678")){
+                prevId = id.toString();
+                return(
+                    <div className='sender'>
+                        <img src={photo} alt='' />
+                        <p>{message.content}</p>
+                    </div>
+                )
+            }else if(message.sender === id.toString() && prevId === id.toString()){
+                prevId = id.toString();
+                return(
+                    <div className='sender' style={{paddingLeft : "50px", marginTop: "0px"}}>
+                        <p>{message.content}</p>
+                    </div>
+                )
+            }else if(message.sender === "5678" && (prevId === " " || prevId === id.toString())){
+                prevId = "5678";
+                return(
+                    <div className='receiver'>
+                        <p>{message.content}</p>
+                        <img src={photo} alt='' />
+                    </div>
+                )
+            }else if(message.sender === "5678" && prevId === "5678"){
+                prevId = "5678";
+                return(
+                    <div className='receiver' style={{paddingRight : "50px", marginTop: "0px"}}>
+                        <p>{message.content}</p>
+                    </div>
+                )
+            }else{
+                return <p>No messages</p>
+            }
+        })}
         </div>
         <div className='right-footer'>
             <form>
-                <input name='message' value='message' placeholder='Message ... ' />
+                <input name='message' placeholder='Message ... ' />
                 <button type='submit'>send</button>
             </form>
         </div>
