eeab403b7140011da51ae589cba6e416bcd34d6e
Himanshu Sah <himanshusah41@gmail.com>
2024-02-12 19:53:50 -0500

    Uploading files

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Header from './components/layouts/Header';
 import ErrorMessage from './components/layouts/ErrorMessage';
 import Login from './components/accounts/Login';
 import Profile from './components/accounts/Profile';
+import WithPrivateRoute from './utils/WithPrivateRoute';
 
 function App() {
   return (
@@ -17,7 +18,11 @@ function App() {
         <Routes>
           <Route exact path='/register' element = {<Register />} />
           <Route exact path='/login' element = {<Login />} />
-          <Route exact path='/profile' element={<Profile />} />
+          <Route exact path='/profile' element={
+          <WithPrivateRoute>
+            <Profile />
+          </WithPrivateRoute>
+          } />
         </Routes>
       </Router>
     </AuthProvider>
