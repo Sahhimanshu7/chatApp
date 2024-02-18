b18fd072ffa707c80a9a0066930ef19bff8e3a32
Himanshu Sah <himanshusah@Himanshus-Air.lan>
2024-02-18 18:59:28 -0500

    Search styles

--- client/src/components/chat/SearchUsers.jsx
+++ client/src/components/chat/SearchUsers.jsx
@@ -1,20 +1,18 @@
 import { SearchIcon } from "@heroicons/react/solid";
+import "../../assests/search.css";
 
 export default function SearchUsers({ handleSearch }) {
-    return(
-        <div className = "search">
-            <SearchIcon 
-            className="search-icon"
-            aria-hidden="true"
-            />
-            <input 
-            id = "search"
-            name = "search"
-            className = "search-box"
-            placeholder = "Search"
-            type = "search"
-            onChange={(e) => handleSearch(e.target.value)}
-            /> 
-        </div>
-    )
+  return (
+    <div className="search">
+      <SearchIcon className="search-icon" aria-hidden="true" />
+      <input
+        id="search"
+        name="search"
+        className="search-box"
+        placeholder="Search"
+        type="search"
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+    </div>
+  );
 }
