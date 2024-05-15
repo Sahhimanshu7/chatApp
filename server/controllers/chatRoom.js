dfa5a3c3e21526a5e1ad638e20fd85698bed8cec
Sahhimanshu7 <himanshusah41@gmail.com>
2024-05-15 09:45:48 -0400

    Chat

--- server/controllers/chatRoom.js
+++ server/controllers/chatRoom.js
@@ -1,4 +1,4 @@
-import ChatRoom from "../models/chatRoom.js";
+import ChatRoom from "../models/ChatRoom.js";
 
 export const createChatRoom = async (req,res) =>{
     const newChatRoom = new ChatRoom({
