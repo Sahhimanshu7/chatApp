f58f773cdc07d7ceededda265a97cb1f4a422bc7
Sahhimanshu7 <himanshusah41@gmail.com>
2024-05-15 09:45:48 -0400

    All-6

--- server/index.js
+++ server/index.js
@@ -15,7 +15,12 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors({origin: "http://www.chatusa.online"}));
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}));
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
